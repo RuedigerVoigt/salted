c33f39517ff19cd58eae2f1f9cd0859d57524b3f
Rüdiger Voigt <ruedigervoigt@gmx.net>
2020-11-22 21:16:03 +0100

    Add check if the Python version running >= 3.8

--- salted/__init__.py
+++ salted/__init__.py
@@ -8,6 +8,12 @@ Source: https://github.com/RuedigerVoigt/salted
 (c) 2020 by Rüdiger Voigt
 Released under the Apache License 2.0
 """
+import sys
+
+if sys.version_info.major != 3 or sys.version_info.minor < 8:
+    raise RuntimeError('You need at least Python 3.8 to run salted. ' +
+                       'You are running: ' + str(sys.version_info.major) +
+                       '.' + str(sys.version_info.minor))
 
 from salted.__main__ import Salted
 
